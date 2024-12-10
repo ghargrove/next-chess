900d3c1017bb7937fb150716169ed01305e08e5c
Graham Hargrove <graham@walkofff.com>
2024-12-10 18:52:34 -0500

    Interview solution

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+
+'use client'
+
+import React, { useRef, useState } from 'react'
+
+type T = 'black-square' | 'white-square'
+
+const Checkerboard: React.FC <{ clicks: Record<string, number>, onItemClick: (v: [number, number]) => void }>= ({ clicks, onItemClick }) => {
+  const rows  = []
+  for (let i = 1; i <= 8; i++) {
+    // Nested loop
+    const column: Array<T> = [];
+    for (let j = 1; j <= 8; j++) {
+      column.push(j % 2 === 0 ? (i % 2 === 0 ? 'white-square' : 'black-square') : (i % 2 === 0 ? 'black-square' : 'white-square'))
+    }
+
+    rows.push(column)
+  }
+
+  return (
+    <div className="checkerboard">
+      {
+        rows.map((row, rowIdx) => {
+          return row.map((col, colIdx) => {
+            const k = `${rowIdx}-${colIdx}`
+
+            return <div key={rowIdx + '-' + colIdx} className={`square ${col}`} onClick={() => onItemClick([rowIdx, colIdx])} >{clicks[k] === undefined ? 0 : clicks[k]}</div>
+          })
+        })
+      }
+    
+    </div>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const totalCountRef = useRef(0)
+  const [clickMatrix, setClickMatrix] = useState<Record<string, number>>({})
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleCellClick: (coords: [number, number]) => void = ([r, c]) => {
+    setClickMatrix((currentClicks) => {
+      const k = `${r}-${c}`
+      const nextValue = currentClicks[k] === undefined ? 1 : currentClicks[k] + 1
+
+      // Update the ref if the next value is higher than the current
+      if (nextValue > totalCountRef.current) {
+        totalCountRef.current = nextValue
+      }
+
+      // Update the state
+      return {
+        ...currentClicks,
+        [k]: nextValue
+      }
+    })
+  }
+
+
+  return (
+    <div className="layout">
+      <div>{totalCountRef.current}</div>
+      <Checkerboard clicks={clickMatrix} onItemClick={handleCellClick} />
+      
     </div>
   );
 }
