218a3ff292091b2602a00f1610795c951eaa2e5e
Graham Hargrove <graham@walkofff.com>
2024-12-10 23:24:00 -0500

    Added check to see if position is along the left boundary

--- app/boundary-checks.test.ts
+++ app/boundary-checks.test.ts
@@ -1,6 +1,6 @@
 import { expect, it } from "@jest/globals";
 
-import { isOnLeftBoundary } from "./boundary-checks";
+import { isOnLeftBoundary, isOnRightBoundary } from "./boundary-checks";
 
 it("returns true if the position is along the left border", () => {
   [0, 8, 16, 24, 32, 40, 48, 56].forEach((v) => {
@@ -11,5 +11,17 @@ it("returns true if the position is along the left border", () => {
 it("returns false if the position is not along the left border", () => {
   [4, 31, 60, 100, -1, 36].forEach((v) => {
     expect(isOnLeftBoundary(v)).toBe(false);
+  });
+});
+
+it("returns true if the position is along the right border", () => {
+  [7, 15, 23, 31, 39, 47, 55, 63].forEach((v) => {
+    expect(isOnRightBoundary(v)).toBe(true);
+  });
+});
+
+it("returns false if the position is not along the right border", () => {
+  [4, 24, 60, 100, -1, 36].forEach((v) => {
+    expect(isOnRightBoundary(v)).toBe(false);
   });
 });
--- app/boundary-checks.ts
+++ app/boundary-checks.ts
@@ -3,3 +3,8 @@
 export function isOnLeftBoundary(position: number): boolean {
   return [0, 8, 16, 24, 32, 40, 48, 56].includes(position)
 }
+
+/** Determines if the `position` is along the right boundary of the board */
+export function isOnRightBoundary(position: number): boolean {
+  return [7, 15, 23, 31, 39, 47, 55, 63].includes(position)
+}
