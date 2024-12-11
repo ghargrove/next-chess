3835d97b1bb89b5c511980e7af3c270ff3b5f072
Graham Hargrove <graham@walkofff.com>
2024-12-11 01:34:49 -0500

    Clear highlighted moves after a drop

--- app/components/Gameboard.tsx
+++ app/components/Gameboard.tsx
@@ -14,7 +14,11 @@ interface GameboardProps {
   /** Describes where on the board the pieces are positioned */
   piecePositions: Partial<Record<PieceId, number>>;
   /** Update the piece to the `position` */
-  onPiecePositionChange: (pieceId: PieceId, position: number, captures?: PieceId) => void;
+  onPiecePositionChange: (
+    pieceId: PieceId,
+    position: number,
+    captures?: PieceId
+  ) => void;
 }
 
 /** Presents a gameboard */
@@ -27,7 +31,7 @@ export const Gameboard: React.FC<GameboardProps> = (props) => {
   } = props;
 
   // Tracks which cells are legal moves during a drag operation
-  const [legalMoves, setLegalMoves] = useState<number[]>([])
+  const [legalMoves, setLegalMoves] = useState<number[]>([]);
 
   const invertedPieces = Array.from(
     Object.entries(piecePositions) as [PieceId, number][]
@@ -59,22 +63,30 @@ export const Gameboard: React.FC<GameboardProps> = (props) => {
   }, []);
 
   const handleDrag: React.DragEventHandler<HTMLDivElement> = (evt) => {
-    evt.dataTransfer.dropEffect = "move";
-
     // Add the piece id to the event
     const { pieceId } = evt.currentTarget.dataset;
     if (pieceId !== undefined) {
+      const pieceBelongsToTurn =
+        (currentTurn === "black" && /^blk/.test(pieceId)) ||
+        (currentTurn === "white" && /^wh/.test(pieceId));
+
+      // Do nothing if the piece doesn't belong to the current turn
+      if (!pieceBelongsToTurn) {
+        return;
+      }
+
+      evt.dataTransfer.dropEffect = "move";
       evt.dataTransfer.setData("text/plain", pieceId);
 
-      setLegalMoves(calculateGamePieceMoves(pieceId as PieceId, piecePositions))
-    }    
+      setLegalMoves(
+        calculateGamePieceMoves(pieceId as PieceId, piecePositions)
+      );
+    }
   };
 
   const handleDragOver: (
     pos: number
   ) => React.DragEventHandler<HTMLDivElement> = (pos) => (evt) => {
-    // console.log("drag over -->", pos);
-
     evt.preventDefault();
   };
 
@@ -91,10 +103,13 @@ export const Gameboard: React.FC<GameboardProps> = (props) => {
 
       // If the piece was dropped on a legal space then update the game board
       if (legalMoves.includes(pos) && pieceBelongsToTurn) {
-        const pieceCaptured = pieceMap.get(pos)
-        
+        const pieceCaptured = pieceMap.get(pos);
+
         onPiecePositionChange(pieceId, pos, pieceCaptured);
       }
+
+      // Clear the highlighted moves
+      setLegalMoves([]);
     };
 
   return (
@@ -109,7 +124,9 @@ export const Gameboard: React.FC<GameboardProps> = (props) => {
           return (
             <div
               key={rowIdx + "-" + colIdx}
-              className={`square ${col} ${legalMoves.includes(boardPos) ? 'highlight-square' : ''}`}
+              className={`square ${col} ${
+                legalMoves.includes(boardPos) ? "highlight-square" : ""
+              }`}
               onDragOver={handleDragOver(boardPos)}
               onDrop={handleDrop(boardPos)}
             >
