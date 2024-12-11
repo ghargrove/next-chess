e5216740f4cdd20b188027456674855f52a19610
Graham Hargrove <hargrove.g.s@gmail.com>
2024-12-11 00:36:08 -0500

    Make sure pawns can't capture pieces directly in front of them (#10)
    Co-authored-by: Graham Hargrove <graham@walkofff.com>

--- app/move-options.test.ts
+++ app/move-options.test.ts
@@ -64,6 +64,16 @@ it("does not allow a black pawn to move forward an capture a pieceoff the right 
   expect(calculateGamePieceMoves("blk-p8", state)).toEqual([31]);
 });
 
+it("does not allow a black pawn to move forward an capture a pieceoff straight ahead", () => {
+  const state = {
+    ...initialState,
+    'blk-p8': 23,
+    'wh-p8': 31,
+  };
+
+  expect(calculateGamePieceMoves("blk-p8", state)).toEqual([]);
+});
+
 it("allows a white pawn to move forward", () => {
   expect(calculateGamePieceMoves("wh-p1", initialState)).toEqual([40]);
 });
@@ -122,4 +132,13 @@ it("does not allow a white pawn to move forward an capture a pieceoff the right 
   };
 
   expect(calculateGamePieceMoves("wh-p8", state)).toEqual([39]);
+});
+
+it("does not allow a white pawn to move forward an capture a pieceoff straight ahead", () => {
+  const state = {
+    ...initialState,
+    'blk-p4': 43,
+  };
+
+  expect(calculateGamePieceMoves("wh-p4", state)).toEqual([]);
 });
