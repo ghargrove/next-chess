3fd2d94342b854ee386edab8837fd18e44383911
Graham Hargrove <graham@walkofff.com>
2024-12-10 23:01:32 -0500

    Added a test to see if a white pawn could capture to their left

--- app/move-options.test.ts
+++ app/move-options.test.ts
@@ -1,11 +1,23 @@
 import { expect, it } from "@jest/globals";
 
+import { initialState } from "./data";
 import { calculateGamePieceMoves } from './move-options'
 
-it("allows a black pawn to move forward", () => {
-  expect(calculateGamePieceMoves('blk-p1', 48)).toEqual([40]);
-});
+// it("allows a black pawn to move forward", () => {
+//   expect(calculateGamePieceMoves('blk-p1', initialState)).toEqual([16]);
+// });
 
-it("allows a white pawn to move forward", () => {
-  expect(calculateGamePieceMoves('wh-p1', 8)).toEqual([16]);
+
+
+// it("allows a white pawn to move forward", () => {
+//   expect(calculateGamePieceMoves('wh-p1', initialState)).toEqual([40]);
+// });
+
+it("allows a white pawn to move forward an capture another piece to its left", () => {
+  const state = {
+    ...initialState,
+    'blk-p5': 44
+  }
+
+  expect(calculateGamePieceMoves('wh-p6', state)).toEqual([44, 45]);
 });
--- app/move-options.ts
+++ app/move-options.ts
@@ -1,32 +1,70 @@
-
 import { PieceId } from "./components/GamePiece";
 
-const pawnRegex = /^(blk|wh)-p[1-8]$/
+const colorRegEx = /^(blk|wh)-.*$/
+const pawnRegex = /^(blk|wh)-p([1-8])$/;
 
 /** Calculate the spaces a piece can move to */
-export function calculateGamePieceMoves(pieceId: PieceId, currentPosition: number): number[] {
-
-  const pawnMatch = pawnRegex.exec(pieceId)
-  if (pawnMatch !== null) {
-    const [_, color] = pawnMatch
-    return movePawn(color as 'blk' | 'wh', currentPosition);
+export function calculateGamePieceMoves(
+  pieceId: PieceId,
+  gameState: Record<PieceId, number>
+): number[] {
+  if (pawnRegex.test(pieceId)) {
+    return movePawn(pieceId, gameState);
   }
 
-  return []
+  return [];
 }
-
 
 /** Determines which postitions a pawn can move to */
-function movePawn(color: 'blk' | 'wh', currentPosition: number) {
-  if (color === 'blk') {
-    return [currentPosition - 8]
+function movePawn(pieceId: PieceId, gameState: Record<PieceId, number>) {
+  const currentPosition = gameState[pieceId]
+  const pawnMatch = pawnRegex.exec(pieceId)
+
+  // This should not happen. Make sure we handle this
+  if (pawnMatch === null) {
+    throw new Error('No pawn match found')
   }
 
-  if (color === 'wh') {
-    return [currentPosition + 8]
+  const [_, color] = pawnMatch
+
+  const invertedPieces = new Map(Array.from(
+    Object.entries(gameState) as [PieceId, number][]
+  ).map<[number, PieceId]>(([k, v]) => [v, k]));
+
+  const pieceMap = new Map(invertedPieces);
+
+  if (color === "blk") {
+    return [currentPosition + 8];
   }
 
-  throw new Error('Invalid color provided');
+  if (color === "wh") {
+    const onePositionForward = currentPosition - 8
+    const positions = [currentPosition - 8]
+
+    // TODO: Deterine if their at a left boundary, right boundary, top boundary, bottom bondary
+
+    // TODO: Determine if they're at the left boundary
+    const pieceToLeft = invertedPieces.get(onePositionForward - 1)
+    if (pieceToLeft) {
+      const x = colorRegEx.exec(pieceToLeft)
+
+      if (x !== null) {
+        const [_, leftPieceColor] = x
+
+        // If it's not the same color, we can move there
+        if (leftPieceColor !== color) {
+          positions.push(onePositionForward - 1)
+        }
+      }
+
+      // Parse it. They can only capture the other players pieces
+    }
+
+
+    return positions.sort();
+  }
+
+  throw new Error("Invalid color provided");
 }
 
-
+function moveRook(currentPosition: number) {}
