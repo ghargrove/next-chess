aee204a75558dafe949a43bf992c7983f53ce071
Graham Hargrove <graham@walkofff.com>
2024-12-11 01:05:52 -0500

    Created captured piece sections in the dashboard

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,8 +1,28 @@
+
+import { PieceId } from "./GamePiece";
+import { GamePiece } from "./GamePiece";
+
+interface DashboardProps {
+  capturesPieces: PieceId[]
+}
+
 /** Presents game information */
 export const Dashboard: React.FC = () => {
+  
+  // Group them
+
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">Chess</h1>
+      <div className="dashboard-captures">
+        <div className="white-captures">
+          <h2>White</h2>
+        </div>
+
+        <div className="black-captures">
+          <h2>Black</h2>
+        </div>
+      </div>
     </div>
   );
 };
