098b18b137622cc1427777a6a6cd2c4aab3a5e1d
Graham Hargrove <graham@walkofff.com>
2024-12-12 15:33:41 -0500

    Removed board debug

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,6 @@ export default function Home() {
     <div className="layout">
       <div>
         <Gameboard
-          debug
           currentTurn={turn}
           piecePositions={activePieces}
           onPiecePositionChange={handlePiecePositionChange}
